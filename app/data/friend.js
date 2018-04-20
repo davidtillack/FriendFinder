c7ca54c40a5daceef34e57f0e1d5af2f26ed3f42
David <dstillac@ncsu.edu>
2018-04-20 13:12:15 -0400

    Still troubleshooting...

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,7 +1,9 @@
 var array = [
   {
     name: "John",
-    photo: "",
+    photo:
+      "https://www.redbubble.com/people/dinkleburg43/works/21859416-donald-trump-face-cutout?p=sticker",
+    description: "I am John and I like stuff",
     scores: ["3", "2", "3", "3", "3", "3", "3", "3", "3", "3"]
   }
 ];
