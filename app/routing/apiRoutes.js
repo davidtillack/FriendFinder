c8a7d3b8289ea8ff6a2de550952cde54a70e5016
David <dstillac@ncsu.edu>
2018-04-20 14:45:53 -0400

    Lets try this...

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,33 +6,25 @@ module.exports = function(app) {
   });
 
   app.post("/api/friend", function(req, res) {
-    var friendScores = req.body.scores;
-    var scoresArray = [];
-    var friendFinderMatch = 0;
+    var friendMatch = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: JSON.parse(req.body.scores)
+    };
 
-    // Run through list of existing friends
-    for (var i = 0; array.length; i++) {
-      var scoreDifferences = 0;
-      for (var z = 0; z < friendScores.length; z++) {
-        // Take the difference between two user's scores to see how close they are
-        scoreDifferences += Math.abs(
-          parseInt(array[i].scores[z]) - parseInt(friendScores[z])
-        );
-      }
-      // Push result into "scoresArray"
-      scoresArray.push(scoreDifferences);
-    }
-    // Compare all now to find best match
-    for (var i = 0; i < scoresArray.length; i++) {
-      if (scoresArray[i] <= scoresArray[friendFinderMatch]) {
-        friendFinderMatch = i;
-      }
-    }
-    // friend match data
-    var yourFriend = array[friendFinderMatch];
-    res.json(yourFriend);
+    var compareArray = [];
 
-    // Push new friend match into array
-    array.push(yourFriend);
+    array.forEach(function(item, index) {
+      var diff = 0;
+      for (var x = 0; item.score.length; x++) {
+        diff += Math.abs(item.scores[x] - friendMatch.scores[x]);
+        compareArray.push({ difference: diff, index: index });
+      }
+    });
+    compareArray.sort(function(friend1, friend2) {
+      return friend1.diff - friend2.diff;
+    });
+    array.push(friendMatch);
+    res.json(array[compareArray[0].index]);
   });
 };
