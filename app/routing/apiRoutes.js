d422dee42143649808dcaee376606000ad909471
David <dstillac@ncsu.edu>
2018-04-21 14:54:35 -0400

    test

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,26 +8,25 @@ module.exports = function(app) {
   });
 
   app.post("/api/friend", function(req, res) {
-       
     var userData = {
-        name: req.body.name,
-        photo: req.body.photo,
-        scores: JSON.parse(req.body.scores)
-        };
-    
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: JSON.parse(req.body.scores)
+    };
+
     var friendArray = [];
 
     friend.forEach(function(question, index) {
-        var difference = 0;
-        for (var i = 0; i < question.score.length; i++) {
-            difference += Math.abs(question.scores[i] - userData.scores[i]);
-            friendArray.push({"difference": difference, "index": index});           
-        }
+      var difference = 0;
+      for (var i = 0; i < question.score.length; i++) {
+        difference += Math.abs(question.scores[i] - userData.scores[i]);
+        friendArray.push({ difference: difference, index: index });
+      }
     });
-    friendArray.sort(function(friend1, friend2){
-        return friend1.difference - friend2.difference;
+    friendArray.sort(function(friend1, friend2) {
+      return friend1.difference - friend2.difference;
     });
     friend.push(userData);
     res.json(friend[friendArray[0].index]);
-    });
-
+  });
+};
