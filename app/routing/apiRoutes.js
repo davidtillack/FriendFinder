d9a6525ac7d3614fce9377ba03177616cdb80688
David <dstillac@ncsu.edu>
2018-04-21 16:05:17 -0400

    test

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 
   app.post("/api/friend", function(req, res) {
     var friendMatch = 0;
-    var diff = 500;
+    var diff = 0;
 
     for (var i = friend.length - 1; i >= 0; i--) {
       var totalDifference = 0;
@@ -18,7 +18,7 @@ module.exports = function(app) {
         totalDifference =
           totalDifference + Math.abs(friend[i].scores[j] - req.body.scores[j]);
       }
-      if (totalDifference < diff) {
+      if (totalDifference > diff) {
         diff = totalDifference;
         friendMatch = i;
       }
