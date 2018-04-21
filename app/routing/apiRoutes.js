a458cbe748ed373ad7fe3b14d4589958582efb0e
David <dstillac@ncsu.edu>
2018-04-21 10:33:47 -0400

    Maybe?

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,9 +14,7 @@ module.exports = function(app) {
       var scoreDifferences = 0;
       for (var z = 0; z < friendScores.length; z++) {
         // Take the difference between two user's scores to see how close they are
-        scoreDifferences += Math.abs(
-          parseInt(friend[i].scores[z]) - parseInt(friendScores[z])
-        );
+        scoreDifferences += Math.abs(friend[i].scores[z] - friendScores[z]);
       }
       // Push result into "scoresArray"
       scoresArray.push(scoreDifferences);
